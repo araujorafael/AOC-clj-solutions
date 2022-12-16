ee427d40c2ff2369fb748fb028838e90827a6536
Rafael Araujo <rafa@Rafaels-MacBook-Pro.local>
2022-12-16 18:11:53 -0300

    Add first puzzle solution

---
+++ Day_1/src/day_1/core.clj
@@ -0,0 +1,38 @@
+(ns day-1.core
+  (:require [clojure.java.io :as io]))
+
+(defn read-file [path]
+  (with-open [rdr (io/reader path)]
+    (doall (line-seq rdr))))
+
+(defn calculate-total-cal-per-elf [lines]
+  (loop [[cal & cal-list] lines
+         carring-calories-count 0
+         elfs-calories []]
+    (if-not (seq cal-list)
+      elfs-calories
+      (if-not (seq cal)
+        (recur cal-list 0 (conj elfs-calories carring-calories-count))
+        (recur cal-list
+               (+ carring-calories-count (read-string cal))
+               elfs-calories)))))
+
+
+;; --------
+(defn first-puzzle [calories-per-elf]
+  (reduce
+      (fn [greater value]
+        (if (< greater value)
+          value
+          greater))
+      calories-per-elf))
+
+;;
+(defn second-puzzle [calories-per-elf])
+
+
+(defn -main [file-path]
+  (let [raw-data (read-file file-path)
+        calories-per-elf (calculate-total-cal-per-elf raw-data)
+        first-puzzle-solution (first-puzzle calories-per-elf)]
+    (println "First Puzzle: " first-puzzle-solution)))
